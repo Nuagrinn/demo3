5811a2bf465397c2116f9681960e175de73034e2
Vladislav <megoboom17@gmail.com>
2024-12-23 17:09:32 +0300

    LongestPalindromicSubstring

---
+++ src/main/java/org/solutions/LongestPalindromicSubstring.java
@@ -0,0 +1,35 @@
+package org.solutions;
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(findP("aaaa"));
+    }
+
+    public static String findP(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);     // Odd length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
